ad16da5f25cb78b3d477b8c22ba82e6e9b1619a2
Adrian <adriansanduta@gmail.com>
2025-03-08 18:30:50 -0600

    pomodoro 1.4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let startTime;
 const countdown = 25 * 60 * 1000;
 const timerMilliseconds = document.querySelector('.timer__milliseconds');
 const timerSeconds = document.querySelector('.timer__seconds');
+const timerMinutes = document.querySelector('.timer__minutes');
 
 function startTimer() {
   startTime = Date.now();
@@ -20,13 +21,28 @@ function resetTimer() {
 function updateTimer() {
   let millisElapsed = Date.now() - startTime;
 
-  let timeLeftInMs = countdown - millisElapsed;
-  let secondsLeft = timeLeftInMs / 1000;
+  let millisLeft = countdown - millisElapsed;
+  let secondsLeft = millisLeft / 1000;
+  let minutesLeft = secondsLeft / 60;
 
-  let millisText = timeLeftInMs % 1000;
+  let millisText = millisLeft % 1000;
   let secondsText = Math.floor(secondsLeft) % 60;
+  let minutesText = Math.floor(minutesLeft);
+
+  if (minutesText.toString().length < 2) {
+    minutesText = minutesText.toString().padStart(2, '0');
+  }
+   
+    if (secondsText.toString().length < 2 ) {
+        secondsText = secondsText.toString().padStart(2, '0');
+    }
+
+    if (millisText.toString().length < 3) {
+        millisText = millisText.toString().padStart(2, '0');
+    }
 
   timerMilliseconds.innerHTML = millisText;
   timerSeconds.innerHTML = secondsText;
+  timerMinutes.inneHTML = minutesText;
   cancelId = requestAnimationFrame(updateTimer);
 }
