cfe53807f8efd08d0608d88071fd1cbcf88861b4
Adrian <adriansanduta@gmail.com>
2025-03-08 18:23:21 -0600

    pomodoro 1.3

--- app.js
+++ app.js
@@ -5,24 +5,28 @@ const timerMilliseconds = document.querySelector('.timer__milliseconds');
 const timerSeconds = document.querySelector('.timer__seconds');
 
 function startTimer() {
-  let startTime = Date.now();
-  cancelId = requestAnimationFrame(updateTimer)
+  startTime = Date.now();
+  cancelId = requestAnimationFrame(updateTimer);
 }
 
 function stopTimer() {
- cancelAnimationFrame(cancelId)
+  cancelAnimationFrame(cancelId);
 }
 
 function resetTimer() {
-
+  
 }
 
 function updateTimer() {
-    let millisElapsed = Date.now() - startTime;
-    let timeLeft = countdown - millisElapsed;
-    let secondsLeft = timeLeftInMs / 1000;
-    let millisText = timeLeft % 1000;
-    timerMilliseconds.innerHTML = millisText;
-    timerSeconds.innerHTML = Math.floor(secondsLeft) % 60;
- cancelId = requestAnimationFrame(updateTimer)
+  let millisElapsed = Date.now() - startTime;
+
+  let timeLeftInMs = countdown - millisElapsed;
+  let secondsLeft = timeLeftInMs / 1000;
+
+  let millisText = timeLeftInMs % 1000;
+  let secondsText = Math.floor(secondsLeft) % 60;
+
+  timerMilliseconds.innerHTML = millisText;
+  timerSeconds.innerHTML = secondsText;
+  cancelId = requestAnimationFrame(updateTimer);
 }
