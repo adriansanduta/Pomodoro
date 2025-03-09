e56b68157ed3393eadb27ebc05f4d1b8178598d9
Adrian <adriansanduta@gmail.com>
2025-03-08 18:52:22 -0600

    pomodoro 1.6

--- app.js
+++ app.js
@@ -5,25 +5,48 @@ const countdown = 25 * 60 * 1000;
 const timerMilliseconds = document.querySelector('.timer__milliseconds');
 const timerSeconds = document.querySelector('.timer__seconds');
 const timerMinutes = document.querySelector('.timer__minutes');
+const startButton = document.querySelector('.stopwatch__start');
+const stopButton = document.querySelector('.stopwatch__stop');
+const resetButton = document.querySelector('.stopwatch__reset');
+
 
 function startTimer() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = false;
   startTime = Date.now();
   cancelId = requestAnimationFrame(updateTimer);
 }
 
 function stopTimer() {
-  savedTime = Date.now() - startTime + savedTime;
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+    
+  savedTime += Date.now() - startTime;
   cancelAnimationFrame(cancelId);
 }
 
 function resetTimer() {
+    startTime = Date.now();
+    savedTime = 0;
 
+    timerMilliseconds.innerHTML = "000";
+    timerSeconds.innerHTML = "05";
+    timerMinutes.innerHTML = "01";
 }
 
 function updateTimer() {
   let millisElapsed = Date.now() - startTime + savedTime;
 
   let millisLeft = countdown - millisElapsed;
+
+  if (millisLeft < 0) {
+    millisLeft = 0;
+    cancelAnimationFrame(cancelId);
+    cancelId = null;
+    }
+
   let secondsLeft = millisLeft / 1000;
   let minutesLeft = secondsLeft / 60;
 
@@ -46,5 +69,8 @@ function updateTimer() {
   timerMilliseconds.innerHTML = millisText;
   timerSeconds.innerHTML = secondsText;
   timerMinutes.innerHTML = minutesText;
-  cancelId = requestAnimationFrame(updateTimer);
+
+  if (cancelId) {
+    cancelId = requestAnimationFrame(updateTimer);
+  }
 }
